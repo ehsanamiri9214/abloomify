4c8f60d46da6cfbec4db1cc759369bd3a8e50978
Ehsan Amiri <ehsanamiri9214@gmail.com>
2024-10-22 13:32:54 -0230

    -

--- src/app/projects/[projectId]/tasks/new/page.tsx
+++ src/app/projects/[projectId]/tasks/new/page.tsx
@@ -1,25 +1,42 @@
 import { createTask } from "@/app/_actions/actions";
 
-const NewTask = () => {
+const NewProjectPage = () => {
   return (
-    <form action={createTask}>
-      <input
-        type="text"
-        name="title"
-        id="title"
-        placeholder="Title"
-        className=""
-      />
-      <input
-        type="text"
-        name="details"
-        id="details"
-        placeholder="Details"
-        className=""
-      />
-      <button type="submit">submit</button>
-    </form>
+    <>
+      <h1 className="text-center capitalize my-4 text-lg">
+        create new task
+      </h1>
+      <form
+        action={createTask}
+        className="flex flex-col max-w-lg m-auto p-2"
+      >
+        <input
+          type="text"
+          name="title"
+          id="title"
+          placeholder="Title"
+          className="p-4 border-b mb-1 rounded-lg"
+          required
+        />
+        <textarea
+          name="details"
+          id="details"
+          placeholder="Details ..."
+          className="p-4 border-b mb-1 rounded-lg"
+          required
+          rows={5}
+          maxLength={280}
+          autoComplete="on"
+        />
+        <button
+          type="submit"
+          className="p-4 bg-sky-500 text-white capitalize rounded-lg"
+        >
+          submit
+        </button>
+      </form>
+    </>
   );
 };
 
-export default NewTask;
+export default NewProjectPage;
